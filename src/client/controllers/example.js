17aa7532800c46e5bdb777466aff82e0bd2927d0
Leandro Leal Parente <leal.parente@gmail.com>
2016-11-24 19:15:22 -0200

    Socket io events

--- src/client/controllers/example.js
+++ src/client/controllers/example.js
@@ -1,9 +1,13 @@
 'use strict';
 
-Application.controller('ExampleController', function($rootScope, $scope, requester) {
+Application.controller('ExampleController', function($rootScope, $scope, $timeout, requester) {
 
 	requester._get('example', function(exampleData) {
 		$scope.data = exampleData;
 	});
+
+	$timeout(function(){
+		$scope.data.title = 'Opa';
+	},3000);
 
 });
