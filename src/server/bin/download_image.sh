f43f6110d7b616b2bac50616c9ec11e3549fe66c
Leandro <leandro@localhost.localdomain>
2019-03-06 11:08:04 -0300

    Image contrast using cv2

--- src/server/bin/download_image.sh
+++ src/server/bin/download_image.sh
@@ -3,9 +3,18 @@
 GDALTMS_URL="$1"
 WINDOW="$2"
 IMAGE_FILE="$3"
+TYPE="$4"
 
-GDAL_PARAMS="-of PNG -co WORLDFILE=YES -tr 30 30 -projwin $WINDOW"
+BASEDIR=$(dirname "$0")
+
+GDAL_PARAMS="-of PNG -co WORLDFILE=YES -ot Byte -tr 30 30 -projwin $WINDOW"
 CONVER_PARAMS="-auto-level -auto-gamma -channel RGB -contrast-stretch 0.5%x0.5%"
 
 mkdir -p $(dirname $IMAGE_FILE)
-curl -s $GDALTMS_URL | gdal_translate $GDAL_PARAMS /vsistdin/ /vsistdout/ | convert - $CONVER_PARAMS $IMAGE_FILE
+
+if [ "$TYPE" == "magick" ]; then
+	curl -s $GDALTMS_URL | gdal_translate $GDAL_PARAMS /vsistdin/ /vsistdout/ | convert - $CONVER_PARAMS $IMAGE_FILE
+else
+	curl -s $GDALTMS_URL | gdal_translate $GDAL_PARAMS /vsistdin/ $IMAGE_FILE
+	python3 $BASEDIR/enhance_img_clahe.py $IMAGE_FILE
+fi
---
+++ src/server/bin/enhance_img_clahe.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+import osr
+import sys
+
+def adjust_gamma(image, gamma=1.0):
+
+   invGamma = 1.0 / gamma
+   table = np.array([((i / 255.0) ** invGamma) * 255
+      for i in np.arange(0, 256)]).astype("uint8")
+
+   return cv2.LUT(image, table)
+
+def enhance_img(data):
+	
+	r, g, b = cv2.split(data)
+
+	claheSmall = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+	claheBig = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8,8))
+	
+	rl = claheBig.apply(r)
+	gl = claheSmall.apply(g)
+	bl = claheBig.apply(b)
+	limg = cv2.merge((rl, gl, bl))
+	limg = adjust_gamma(limg, 1.5)
+	
+	return limg
+
+def read_img(filename):
+	return cv2.imread(filename)
+
+def write_img(filename, data):
+	cv2.imwrite(filename, data)
+
+if __name__ == "__main__":
+	
+	filename1=sys.argv[1]
+
+	data1 = read_img(filename1)
+	data1 = enhance_img(data1)
+
+	write_img(filename1, data1)
