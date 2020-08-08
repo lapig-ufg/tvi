cbdc3b619051f4c16b6b9e3e0d6eee72295e8a3e
Tharles <irtharles@gmail.com>
2020-08-07 23:42:37 -0300

    Add files via upload

---
+++ devops/scripts/correct_campaign.js
@@ -0,0 +1,23 @@
+db.getCollection('points').find({'campaign':'raisg_ecuador_val'}).forEach(function(point) {
+    var campaign = db.getCollection('campaign').findOne({_id: point.campaign})
+    var numInspections = db.getCollection('campaign').distinct('numInspec',{'_id': point.campaign})
+    
+    if(point.inspection.length != numInspections) {
+      print('Correcting ' + point._id + ' with ' + point.inspection.length + ' inspections')
+      
+      if(point.inspection.length > numInspections) {
+        var numExceededInspection = point.inspection.length - numInspections
+        print(' Removing ' + numExceededInspection + ' inspections')
+
+        var inspection = point.inspection.slice(0, -1*numExceededInspection)
+        var userName = point.userName.slice(0, -1*numExceededInspection)
+
+        point.inspection = inspection
+        point.userName = userName
+      }
+      
+      point.underInspection = point.inspection.length
+      print(' Final number of inspections ' + point.underInspection)
+      db.getCollection('points').save(point)
+    }
+})
