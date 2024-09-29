4db5b527496c95c96aaf82e080d7ea97b97dc879
Tharles de Sousa Andrade <irtharles@gmail.com>
2024-09-29 13:15:12 -0300

    update config deploy 01.

--- src/server/prod-start.sh
+++ src/server/prod-start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-export NODE_ENV=prod; export NODE_NO_WARNINGS=1; nohup node --no-deprecation app-tvi-cluster.js &> app.out &
+export NODE_ENV=prod
+export NODE_NO_WARNINGS=1
+
+# Iniciar o app e redirecionar a saída para o stdout
+nohup node --no-deprecation app-tvi-cluster.js > /dev/stdout 2> /dev/stderr &
