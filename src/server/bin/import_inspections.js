e6b2f92e973bafc444c6c22144049df386970062
Tharles de Sousa Andrade <irtharles@gmail.com>
2025-02-13 15:18:21 -0300

    update import_inspections.js

--- src/server/bin/import_inspections.js
+++ src/server/bin/import_inspections.js
@@ -1,13 +1,5 @@
 const fs = require("fs");
-const async = require("async");
 const mongodb = require('mongodb');
-const exec = require('child_process').exec;
-
-/**
- * Args
- * geojsonFile - path
- * campaign - identification of campaign
- */
 
 const geojsonFile = process.argv[2];
 const campaign = process.argv[3];
@@ -23,31 +15,27 @@ const checkError = function(error) {
 }
 
 const getInspections = function(geojsonDataStr) {
-	const geojsonData = JSON.parse(geojsonDataStr)
+	const geojsonData = JSON.parse(geojsonDataStr);
 
-	let points = []
+	let points = [];
 	for(let i = 0; i < geojsonData.features.length; i++) {
 		const pointProperties = geojsonData.features[i].properties;
 		let inspections = [];
 		Object.keys(pointProperties).forEach(function(key) {
 			if(key.toLowerCase().includes('CLASS_'.toLowerCase())){
-				key = key.toLowerCase()
+				key = key.toLowerCase();
 				inspections.push({
 					"year": parseInt(key.replace('class_','')),
 					"class": pointProperties[key.toUpperCase()]
-				})
+				});
 			}
 		});
 
-
-
-		points.push(
-			{
-				"lon": geojsonData.features[i].geometry.coordinates[0],
-				"lat": geojsonData.features[i].geometry.coordinates[1],
-				"inspections": inspections
-			}
-		);
+		points.push({
+			"lon": geojsonData.features[i].geometry.coordinates[0],
+			"lat": geojsonData.features[i].geometry.coordinates[1],
+			"inspections": inspections
+		});
 	}
 
 	return points;
@@ -56,56 +44,56 @@ const getInspections = function(geojsonDataStr) {
 const getDB = function(dbUrl, callback) {
 	const MongoClient = mongodb.MongoClient;
 	MongoClient.connect(dbUrl, function(err, db) {
-			if(err)
-				return console.dir(err);
-			callback(db);
+		if(err)
+			return console.dir(err);
+		callback(db);
 	});
 }
-
 
 fs.readFile(geojsonFile, 'utf-8', function(error, geojsonDataStr){
 	checkError(error);
 
 	getDB(dbUrl, function(db) {
 		db.collection(collectionPointsName, function(err, collectionPoints) {
-
-			const insertInspections = (point, next) => {
-
-				let inspection = {
-					"counter" : 0,
-					"form" : point.inspections.map((item) =>{
-						return {
-							"initialYear" : item.year,
-							"finalYear" : item.year,
-							"landUse" : item.class
-						}
-					}) ,
-					"fillDate" : new Date()
-				};
-
-				const update = {
-					'$push': {
-						"inspection": inspection,
-						"userName": "Classificação Automática"
-					}
-				};
-			
-				collectionPoints.update({ 'campaign': campaign, 'lon': point.lon, lat: point.lat }, update, function(err, pto) {
-					if(err){
-						console.log("[ERROR] " + JSON.stringify(pto) + " not updated.");
-					} else{
-						console.log("[SUCCESS] " + JSON.stringify(pto) + " updated.");
-					}
-					next();
-				});
-
+			if (err) {
+				console.error(err);
+				db.close();
+				return;
 			}
 
-			const onComplete = function() {
-				db.close();
-			};
+			const points = getInspections(geojsonDataStr);
+			const bulkOps = points.map(point => {
+				const inspection = {
+					"counter": 0,
+					"form": point.inspections.map(item => ({
+						"initialYear": item.year,
+						"finalYear": item.year,
+						"landUse": item.class
+					})),
+					"fillDate": new Date()
+				};
 
-			async.eachSeries(getInspections(geojsonDataStr), insertInspections, onComplete);
+				return {
+					updateOne: {
+						filter: { 'campaign': campaign, 'lon': point.lon, 'lat': point.lat },
+						update: {
+							'$push': {
+								"inspection": inspection,
+								"userName": "Classificação Automática"
+							}
+						}
+					}
+				};
+			});
+
+			collectionPoints.bulkWrite(bulkOps, { ordered: false }, function(err, result) {
+				if (err) {
+					console.error("[ERROR] Bulk write failed:", err);
+				} else {
+					console.log("[SUCCESS] Bulk write completed:", result);
+				}
+				db.close();
+			});
 		});
 	});
 });
