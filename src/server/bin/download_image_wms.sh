eddaf183e46fe6da90da59f362c4d7ebc44c35cd
Tharles de Sousa Andrade <irtharles@gmail.com>
2025-06-11 23:05:32 -0300

    rollback

--- src/server/bin/download_image_wms.sh
+++ src/server/bin/download_image_wms.sh
@@ -1,40 +1,44 @@
-if [ "$#" -lt 4 ]; then
-    echo "Uso: $0 <lon> <lat> <wms_template_url> <image_file> [wms_credentials_base64]"
-    exit 1
-fi
+#!/bin/bash
 
 LON="$1"
 LAT="$2"
-TEMPLATE="$3"
+BBOX=$(bbox --lon $LON --lat $LAT)
+WMS_URL=$(echo $3 | sed "s/{bbox}/$BBOX/")
+
+echo "WMS_URL: $WMS_URL"
+
 IMAGE_FILE="$4"
 WMS_CREDENTIALS="$5"
 
-# Extrai o valor do parâmetro version= da URL
-WMS_VERSION=$(echo "$TEMPLATE" | grep -oP '(?<=version=)[^&]*')
+BASEDIR=$(dirname "$0")
 
-# Default para 1.1.0 caso não encontrado
-if [ -z "$WMS_VERSION" ]; then
-    WMS_VERSION="1.1.0"
-fi
-
-echo "[INFO] WMS version detectada: $WMS_VERSION"
-
-# Executa o bbox com a versão correta
-BBOX=$(bbox --lon "$LON" --lat "$LAT" --wms_version "$WMS_VERSION") || {
-    echo "Erro: comando bbox falhou"
-    exit 1
-}
-
-WMS_URL="${TEMPLATE/\{bbox\}/$BBOX}"
-
-mkdir -p "$(dirname "$IMAGE_FILE")"
+mkdir -p $(dirname "$IMAGE_FILE")
 
 USER_AGENT="Mozilla/5.0 QGIS/32809/KDE Flatpak runtime"
 
-echo "[INFO] URL gerada: $WMS_URL"
+# Função para verificar se o arquivo foi baixado com sucesso
+check_file() {
+    if [ -f "$IMAGE_FILE" ] && [ -s "$IMAGE_FILE" ]; then
+        return 0  # Arquivo existe e não está vazio
+    else
+        return 1  # Arquivo não existe ou está vazio
+    fi
+}
 
+# Baixa a imagem usando curl
 if [ -n "$WMS_CREDENTIALS" ]; then
+    # Com credenciais
     curl -f -s -L -H "User-Agent: ${USER_AGENT}" -H "Authorization: Basic $WMS_CREDENTIALS" "$WMS_URL" > "$IMAGE_FILE"
 else
-    curl -f -s -L -H "User-Agent: ${USER_AGENT}" "$WMS_URL" > "$IMAGE_FILE"
+    # Sem credenciais
+    curl -f -s -L "$WMS_URL" > "$IMAGE_FILE"
+fi
+
+# Verifica se o arquivo foi baixado com sucesso
+if check_file; then
+    echo "Arquivo baixado com sucesso: $IMAGE_FILE"
+    # Executa o script Python para processar a imagem
+     python3 "$BASEDIR/enhance_img_clahe.py" "$IMAGE_FILE"
+else
+    echo "Erro: O arquivo não foi baixado ou está vazio: $IMAGE_FILE"
 fi
